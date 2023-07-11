2d2d29660422ec171485531654832cee3dba3498
Jéssica Vivian <jessicavivian.eng@gmail.com>
2023-07-11 19:13:02 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Amor, te amo! Acho que hoje eu mereço flores. Ri se você concorda!</h1>
+    return <h1>Amor, te amo! Obrigada por me apoiar.</h1>
 }
 
 export default Home;
